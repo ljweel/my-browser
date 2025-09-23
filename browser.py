9cdca8fea42f912a12b2a71e40093f78a17ec54c
JEONGWOO LEE <ljwljw8541@uos.ac.kr>
2025-09-23 18:43:37 +0900

    feat) exercise 1.2
    연습문제 1.2) file scheme 을 지원하도록 추가했습니다.

--- browser.py
+++ browser.py
@@ -1,61 +1,75 @@
-import socket, ssl
+import socket, ssl, urllib.parse
 class URL:
     def __init__(self, url):
-        self.scheme, url = url.split("://", 1)
-        assert self.scheme in ["http", "https"]
-        if self.scheme == "http":
-            self.port = 80
-        elif self.scheme == 'https':
-            self.port = 443
 
-        if '/' not in url:
-            url += '/'
-        self.host, url = url.split('/', 1)
-        if ':' in self.host:
-            self.host, port = self.host.split(':', 1)
-            self.port = int(port)
-        self.path = '/' + url
+        urlinfo = urllib.parse.urlparse(url)
+        
+        self.scheme = urlinfo.scheme
+        self.host = urlinfo.hostname
+        self.path = urlinfo.path
+        if self.scheme == 'file':
+            self.host = None
+            self.path = urlinfo.netloc + urlinfo.path
+            self.path = self.path.lstrip()
+
+        if self.scheme in ['http', 'https'] and urlinfo.port: 
+            self.port = urlinfo.port
+        elif self.scheme == 'http': 
+            self.port = 80
+        elif self.scheme == 'https': 
+            self.port = 443
+        else:
+            self.port = None
     
     def request(self, headers={}):
-        s = socket.socket(
-            family=socket.AF_INET,
-            type=socket.SOCK_STREAM,
-            proto=socket.IPPROTO_TCP,
-        )
-        s.connect((self.host, self.port))
-        if self.scheme == 'https':
-            ctx = ssl.create_default_context()
-            s = ctx.wrap_socket(s, server_hostname=self.host)
-        req_headers = {}
-        req_headers['host'] = self.host
-        req_headers['connection'] = 'close'
-        req_headers['user-agent'] = 'mybrowser/1.0'
+        if self.scheme == 'file':
 
-        for key, values in headers.items():
-            req_headers[key.casefold()] = values
-        req =  f"GET {self.path} HTTP/1.1\r\n"
+            with open(self.path, 'r') as file:
+                body = file.read()
+            return body
+
         
-        for key, value in req_headers.items():
-            req += f'{key}: {value}\r\n'
-        req +=  "\r\n"
-        s.send(req.encode('utf8'))
+        elif self.scheme in  ['http', 'https']:
 
-        response = s.makefile('r', encoding="utf8", newline="\r\n")
-        statusline = response.readline()
-        version, status, explanation = statusline.split(' ', 2)
-        response_header = {}
-        while True:
-            line = response.readline()
-            if line == '\r\n': break
-            header, value = line.split(':', 1)
-            response_header[header.casefold()] = value.strip()
+            s = socket.socket(
+                family=socket.AF_INET,
+                type=socket.SOCK_STREAM,
+                proto=socket.IPPROTO_TCP,
+            )
+            s.connect((self.host, self.port))
+            if self.scheme == 'https':
+                ctx = ssl.create_default_context()
+                s = ctx.wrap_socket(s, server_hostname=self.host)
+            req_headers = {}
+            req_headers['host'] = self.host
+            req_headers['connection'] = 'close'
+            req_headers['user-agent'] = 'mybrowser/1.0'
 
-        assert "transfer-encoding" not in response_header
-        assert "content-encoding" not in response_header
+            for key, values in headers.items():
+                req_headers[key.casefold()] = values
+            req =  f"GET {self.path} HTTP/1.1\r\n"
+            
+            for key, value in req_headers.items():
+                req += f'{key}: {value}\r\n'
+            req +=  "\r\n"
+            s.send(req.encode('utf8'))
 
-        body = response.read()
-        s.close()
-        return body
+            response = s.makefile('r', encoding="utf8", newline="\r\n")
+            statusline = response.readline()
+            version, status, explanation = statusline.split(' ', 2)
+            response_header = {}
+            while True:
+                line = response.readline()
+                if line == '\r\n': break
+                header, value = line.split(':', 1)
+                response_header[header.casefold()] = value.strip()
+
+            assert "transfer-encoding" not in response_header
+            assert "content-encoding" not in response_header
+
+            body = response.read()
+            s.close()
+            return body
     def __repr__(self):
         return "URL(scheme={}, host={}, port={}, path={!r})".format(
             self.scheme, self.host, self.port, self.path)
@@ -75,6 +89,5 @@ def load(url):
     show(body)
 
 if __name__ == '__main__':
-    url = 'http://example.org/'
     import sys
     load(URL(sys.argv[1]))
